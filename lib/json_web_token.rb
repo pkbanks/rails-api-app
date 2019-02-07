ea020cf203b9bb6914db9e3afb0e898b333ff687
PK Banks <pkbanks12@gmail.com>
2019-02-07 11:55:54 -0500

    removal of p and puts

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,9 +2,6 @@ class JsonWebToken
  class << self
    def encode(payload, exp = 24.hours.from_now)
      payload[:exp] = exp.to_i
-     puts "payload:"
-     p payload
-     puts "secret_key_base"
      secret_key_base = Rails.application.credentials.secret_key_base
      JWT.encode(payload, secret_key_base)
    end
